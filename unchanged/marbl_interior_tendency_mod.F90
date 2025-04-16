b8edb58ec2dd4f9bab1f43aa24ed55d2950ae112
jlinmod <134327817+jlinmod@users.noreply.github.com>
2025-04-16 10:29:10 -0400

    Update marbl_interior_tendency_mod_jsl.244.F90

--- unchanged/marbl_interior_tendency_mod.F90
+++ unchanged/marbl_interior_tendency_mod.F90
@@ -3350,14 +3350,13 @@ contains
        zooplankton_derived_terms, dissolved_organic_matter, nitrif, denitrif, sed_denitrif, &
        Fe_scavenge, Lig_prod, Lig_loss, P_iron_remin, POC_remin, POP_remin, P_SiO2_remin, &
        P_CaCO3_remin, P_CaCO3_ALT_CO2_remin, other_remin, PON_remin, tracer_local, &
-       o2_consumption_scalef, o2_production, o2_consumption, interior_tendencies, interior_tendency_forcing_indices)
+       o2_consumption_scalef, o2_production, o2_consumption, interior_tendencies)
 
     integer,                              intent(in)    :: km
     type(marbl_tracer_index_type),        intent(in)    :: marbl_tracer_indices
     type(autotroph_derived_terms_type),   intent(in)    :: autotroph_derived_terms
     type(zooplankton_derived_terms_type), intent(in)    :: zooplankton_derived_terms
     type(dissolved_organic_matter_type),  intent(in)    :: dissolved_organic_matter
-    type(marbl_interior_tendency_forcing_indexing_type),     intent(in)    :: interior_tendency_forcing_indices
     real(r8),                             intent(in)    :: nitrif(km)
     real(r8),                             intent(in)    :: denitrif(km)
     real(r8),                             intent(in)    :: sed_denitrif(km)
